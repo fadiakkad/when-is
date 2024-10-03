ee3f7a771fb43d0dcacc1da093c0467f19b5753c
Mohammad Tello <m11tello@hotmail.com>
2024-10-03 16:52:03 +0300

    removed unnecessary files

--- src/helpers/importImages.js
+++
@@ -1,8 +0,0 @@
-export const importAllImages = (r) => {
-    let images = {};
-    r.keys().forEach((item, index) => {
-      images[item.replace('./', '')] = r(item);
-    });
-    return images;
-  };
-  
